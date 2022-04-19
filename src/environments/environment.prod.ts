b87203dfe0f0d0b7e2898308d61cadd8aca99ef1
atienoobwanda <atienoobwanda@gmail.com>
2022-04-19 09:15:40 +0300

    token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  gitToken: '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',
+  gitToken: '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',
+  gitSecret: '9329d6649d52603b6cc8ca4e5ffc87ed7a05e59c'
 };
